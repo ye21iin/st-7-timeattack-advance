9fe72d2a0e6e4b7b934fd4d4abbda9bbfb681eda
ye21iin <kathykr120@gmail.com>
2025-03-07 12:19:08 +0900

    refactor: Home - useQuery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,16 @@
-import { useState, useEffect } from "react";
 import { todoApi } from "../api/todos";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  // TODO: 필수: useQuery 로 리팩터링 하세요.
-  // TODO: 선택: useQuery 로 리팩터링 후, 커스텀훅 useTodosQuery 로 정리해 보세요.
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchData,
+  });
 
-  const fetchData = async () => {
-    try {
-      const response = await todoApi.get("/todos");
-      setData(response.data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <div style={{ fontSize: 36 }}>로딩중...</div>;
-  }
-
-  if (error) {
-    console.error(error);
-    return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
-    );
-  }
+  if (isPending) return <>Loading...</>;
+  if (isError) return <>Error...</>;
 
   return (
     <>
@@ -44,3 +20,8 @@ export default function Home() {
     </>
   );
 }
+
+const fetchData = async () => {
+  const response = await todoApi.get("/todos");
+  return response.data;
+};
