7763c97982389aedff2963fd551fed9e30d1ebc0
ye21iin <kathykr120@gmail.com>
2025-03-07 12:07:32 +0900

    init: queryClient 선언

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./shared/Router";
 
 function App() {
-  return <Router />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
